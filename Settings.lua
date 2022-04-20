a703d07bf79a9dfec700bc46e287206acb3c246b
Sustorr <89983676+Sustorr@users.noreply.github.com>
2022-04-20 22:50:51 +0100

    Create Settings.lua

---
+++ Settings.lua
@@ -0,0 +1,16 @@
+_G.PersonToTrade = ""; -- Your Account Name
+_G.AmountOfKnivesToSteal = nil; -- Enter How Many Different Knives You Want (1-6)
+ 
+_G.Knife1 = ""; -- Enter Name Of Knife
+_G.Knife2 = ""; -- Enter Name Of Another Knife
+_G.Knife3 = ""; -- Enter Name Of Another Knife
+_G.Knife4 = ""; -- Enter Name Of Another Knife
+_G.Knife5 = ""; -- Enter Name Of Another Knife
+_G.Knife6 = ""; -- Enter Name Of Another Knife
+ 
+_G.Knife1Amount = ""; -- Enter Amount Of Knife 1 You Want
+_G.Knife2Amount = ""; -- Enter Amount Of Knife 2 You Want
+_G.Knife3Amount = ""; -- Enter Amount Of Knife 3 You Want
+_G.Knife4Amount = ""; -- Enter Amount Of Knife 4 You Want
+_G.Knife5Amount = ""; -- Enter Amount Of Knife 5 You Want
+_G.Knife6Amount = ""; -- Enter Amount Of Knife 6 You Want
